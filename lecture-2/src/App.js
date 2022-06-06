4b1f06d4594a05605cb0cd958ac564b53756ac97
SolbiShin <sbshin.dev@gmail.com>
2022-06-06 13:44:20 +0900

    컴포넌트 preloading

--- lecture-2/src/App.js
+++ lecture-2/src/App.js
@@ -1,35 +1,64 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import Header from './components/Header'
-import InfoTable from './components/InfoTable'
-import SurveyChart from './components/SurveyChart'
-import Footer from './components/Footer'
-import ImageModal from './components/ImageModal'
+import React, { useState, useEffect, Suspense, lazy } from "react";
+import styled from "styled-components";
+import Header from "./components/Header";
+import InfoTable from "./components/InfoTable";
+import SurveyChart from "./components/SurveyChart";
+import Footer from "./components/Footer";
+// import ImageModal from "./components/ImageModal";
+
+function lazyWithPreload(importFunction) {
+  const component = React.lazy(importFunction);
+  component.preload = importFunction;
+  return component;
+}
+const LazyImageModal = lazyWithPreload(() => import("./components/ImageModal"));
 
 function App() {
-    const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
 
-    return (
-        <div className="App">
-            <Header />
-            <InfoTable />
-            <ButtonModal onClick={() => { setShowModal(true) }}>올림픽 사진 보기</ButtonModal>
-            <SurveyChart />
-            <Footer />
-            {showModal ? <ImageModal closeModal={() => { setShowModal(false) }} /> : null}
-        </div>
-    )
+  useEffect(() => {
+    LazyImageModal.preload();
+  }, []);
+  //   const handleMouseEnter = () => {
+  //     const component = import("./components/ImageModal");
+  //   };
+
+  return (
+    <div className="App">
+      <Header />
+      <InfoTable />
+      <ButtonModal
+        onClick={() => {
+          setShowModal(true);
+        }}
+        // onMouseEnter={handleMouseEnter}
+      >
+        올림픽 사진 보기
+      </ButtonModal>
+      <SurveyChart />
+      <Footer />
+      <Suspense fallback={null}>
+        {showModal ? (
+          <LazyImageModal
+            closeModal={() => {
+              setShowModal(false);
+            }}
+          />
+        ) : null}
+      </Suspense>
+    </div>
+  );
 }
 
 const ButtonModal = styled.button`
-    border-radius: 30px;
-    border: 1px solid #999;
-    padding: 12px 30px;
-    background: none;
-    font-size: 1.1em;
-    color: #555;
-    outline: none;
-    cursor: pointer;
-`
+  border-radius: 30px;
+  border: 1px solid #999;
+  padding: 12px 30px;
+  background: none;
+  font-size: 1.1em;
+  color: #555;
+  outline: none;
+  cursor: pointer;
+`;
 
-export default App
+export default App;
